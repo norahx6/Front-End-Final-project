df577e3c1007f3ca2f0ff01067ff04c84dd14380
norahx6 <norah.almughera18@gmail.com>
2022-09-11 10:50:43 +0300

    nora

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
@@ -6,7 +5,6 @@ function Header() {
 return(
 
 <div className='Header'>
-{/* <Link to='/' className='titles'>INTUITIVE QUIZ HUB</Link> */}
 <lable className='titles'>INTUITIVE QUIZ HUB</lable>
 <hr className='line' />
 </div>
